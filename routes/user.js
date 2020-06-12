3875313a42282534d89125438bb6321a30877177
chuks-o <chukwualasu123@gmail.com>
2020-06-12 23:35:00 +0100

    Profile:deploy to heroku

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,8 @@ const { verifyToken } = require("../utilities/authUtils");
 
 router.post("/:id", verifyToken, UserController.getUser);
 router.post("/update/:userId", verifyToken, UserController.updateUser);
+router.get("/", function (req, res, next) {
+  return res.status(200).send("Shoplex app");
+});
 
 module.exports = router;
